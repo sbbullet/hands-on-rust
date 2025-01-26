53e502fdd64094956bbd952388637cad8cc2dc43
Santosh Bhattarai <20886273+sbbullet@users.noreply.github.com>
2025-01-26 22:06:23 +0545

    chore: delete target folder

--- hello_cargo/target/debug/deps/hello_cargo-997b84aba90292b6.d
+++
@@ -1,5 +0,0 @@
-/Users/santosh/Development/Personal/hands-on-rust/hello_cargo/target/debug/deps/hello_cargo-997b84aba90292b6: src/main.rs
-
-/Users/santosh/Development/Personal/hands-on-rust/hello_cargo/target/debug/deps/hello_cargo-997b84aba90292b6.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/santosh/Development/Personal/hands-on-rust/hello_cargo/target/debug/hello_cargo: /Users/santosh/Development/Personal/hands-on-rust/hello_cargo/src/main.rs
